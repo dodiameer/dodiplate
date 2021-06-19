17997b129e92b0212ac51e695e7dc5d00cd1eb4c
dodiameer <mtxshiftg@gmail.com>
2021-06-19 17:02:36 +0300

    feat: Working if cmd in same directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use git2::Repository;
+use remove_dir_all::remove_dir_all as rimraff;
 use serde::{Deserialize, Serialize};
+use std::env;
 use std::error::Error;
-use std::io::{stdin, stdout, Write};
 use std::fs;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Boilerplate {
@@ -11,23 +15,44 @@ struct Boilerplate {
 
 impl std::fmt::Display for Boilerplate {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        write!(f, "Boilerplate `{}` at repo `{}`", self.name, self.repo)
+        write!(f, "boilerplate `{}` at repo `{}`", self.name, self.repo)
     }
 }
 
 fn get_boilerplates() -> Result<Vec<Boilerplate>, Box<dyn Error>> {
-    let file = fs::read_to_string("boilerplates.json")?;
+    let file_path = Path::new(&env::current_dir()?).join("boilerplates.json");
+    let file = fs::read_to_string(file_path)?;
     let boilerplates: Vec<Boilerplate> = serde_json::from_str(&file)?;
-    Ok(boilerplates) 
+    Ok(boilerplates)
 }
 
-fn find_boilerplate(boilerplates: Vec<Boilerplate>, name: &str) -> Result<Boilerplate, Box<dyn Error>> {
+fn find_boilerplate(
+    boilerplates: Vec<Boilerplate>,
+    name: &str,
+) -> Result<Boilerplate, Box<dyn Error>> {
     for boilerplate in boilerplates {
         if boilerplate.name.as_str() == name {
             return Ok(boilerplate);
         }
     }
     panic!("boilerplate not found");
+}
+
+fn clone_boilerplate(
+    boilerplate: &Boilerplate,
+    output_directory: &str,
+) -> Result<String, Box<dyn Error>> {
+    let clone_path = Path::new(&env::current_dir()?).join(output_directory);
+    if clone_path.is_dir() && clone_path.exists() {
+        println!(
+            "Output directory `{}` already exists. Removing it...",
+            output_directory
+        );
+        rimraff(&clone_path)?;
+    }
+    Repository::clone(&boilerplate.repo, &clone_path)?;
+    rimraff(clone_path.join(".git"))?;
+    Ok(clone_path.display().to_string())
 }
 
 // This is so overenginnered but I could only get it to work this way
@@ -46,6 +71,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let boilerplates = get_boilerplates()?;
     let name = input("Boilerplate name")?;
     let boilerplate = find_boilerplate(boilerplates, &name.to_lowercase())?;
-    println!("{}", boilerplate);
+    let output_directory = input("Output directory")?;
+    println!("Cloning {}", boilerplate);
+    clone_boilerplate(&boilerplate, &output_directory)?;
+    println!("Cloned! Your new boilerplate is at `{}`", output_directory);
     Ok(())
 }
